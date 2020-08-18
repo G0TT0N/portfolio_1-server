46743ebc207c8e78fe331c39050d1f364572465f
gotton <gotton92@mail.ru>
2020-08-18 15:29:23 +0300

    - connected DB; - created user registration route;

--- index.ts
+++ index.ts
@@ -1,16 +1,29 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+
 const port = process.env.PORT || 4050;
 const cors = require("cors");
 
 app.use(cors());
-app.use(express.json({ extended: true }));
+app.use(express.json({extended: true}));
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use("/auth", require("./routes/auth/auth-routes"));
 
-app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`);
-});
+async function start() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
+    app.listen(port, () => console.log(`STARTED ${port}`));
+  } catch (e) {
+    console.log("server error", e.message);
+    process.exit(1);
+  }
+}
+
+start();
---
+++ routes/auth/auth-routes.ts
@@ -0,0 +1,34 @@
+const {Router} = require("express");
+const User = require("../../models/User");
+const bcrypt = require("bcryptjs");
+
+const router = Router();
+
+// /authApi
+router.post("/register", async (req, res) => {
+  try {
+    const {email, password} = req.body;
+
+    const candidate = await User.findOne({email});
+
+    if (candidate) {
+      return res.status(400).json({message: "user already exist"});
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const user = new User({
+      email,
+      password: hashedPassword,
+      created: new Date(),
+    });
+
+    await user.save();
+
+    res.status(201).json({success: true});
+  } catch (e) {
+    res.status(500).json({message: "new error", e});
+  }
+});
+
+module.exports = router;

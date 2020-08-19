b2a07a8ac97de5fbb8faa117704c4ff4025e4a97
gotton <gotton92@mail.ru>
2020-08-19 09:30:10 +0300

    new start script

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+
 const authRoutes = require("./routes/auth/auth-routes.ts");
 
 const port = process.env.PORT || 4050;

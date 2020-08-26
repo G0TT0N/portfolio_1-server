908e20175a3b9922f945cd37365beb3933af93c6
gotton <gotton92@mail.ru>
2020-08-26 17:22:37 +0300

    - added new route: getUserInfo

--- routes/user/userRoutes.ts
+++ routes/user/userRoutes.ts
@@ -31,5 +31,20 @@ router.post("/createUser", async (req, res) => {
     res.status(500).json(e);
   }
 });
+router.post("/getUserInfo", async (req, res) => {
+  try {
+    const {userId} = req.body;
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json("user not found");
+    }
+
+    res.json({email: user.email, name: user.name, phone: user.phone});
+  } catch (e) {
+    res.status(500).json(e);
+  }
+});
 
 module.exports = router;
